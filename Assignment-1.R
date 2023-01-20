380416b554216f5dd81653bea9ecc40d53383c2d
Spra03 <11s11p11sp@gmail.com>
2023-01-19 23:47:23 -0800

    question 1 complete

--- Assignment-1.R
+++ Assignment-1.R
@@ -6,22 +6,28 @@ load("04cars.rda")
 data = dat[,c(13,15)]
 names(data) = c("hp", "mpg")
 
-
-n = 100
-m = lm(y~x)
-p=1
-df_1 = p+1
-df_2=n-p-1
-fquartile = sqrt((p+1) * qf(conf, df_1,df_2))
-
-y_ = predict(m, data.frame(x=x), se =  T)
-
-
-cb_upper =  (y_$fit + ((fquartile)*y_$se.fit))
-cb_lower =  (y_$fit - ((fquartile)*y_$se.fit))
-
-plot(x,y, type = 'p')
-lines(x,cb_upper, type = 'l', col ="green")
-lines(x,cb_lower, type = 'l', col ="green")
-abline(m, col = 'red', lwd = 2)
+confBand <- function(x,y, conf = 0.95){
   
+  n = 100
+  m = lm(y~x)
+  p=1
+  df_1 = p+1
+  df_2=n-p-1
+  fquartile = sqrt((p+1) * qf(conf, df_1,df_2))
+  
+  y_ = predict(m, data.frame(x=x), se =  T)
+
+  
+  cb_upper =  (y_$fit + ((fquartile)*y_$se.fit))
+  cb_lower =  (y_$fit - ((fquartile)*y_$se.fit))
+  
+  plot(x,y, type = 'p')
+  lines(x,cb_upper, type = 'l', col ="green")
+  lines(x,cb_lower, type = 'l', col ="green")
+  abline(m, col = 'red', lwd = 2)
+  
+  return (list(upb = cb_upper, lwb = cb_lower, y_pred = y_$fit, pred_se = y_$se.fit, Fvalue = fquartile))
+}
+
+confBand(data$hp, data$mpg)
+
