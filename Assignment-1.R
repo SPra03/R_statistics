7ef24550d5c411067f2f1b91cf73910a1d0ebb3f
Spra03 <11s11p11sp@gmail.com>
2023-01-19 23:01:18 -0800

    question 1

---
+++ Assignment-1.R
@@ -0,0 +1,27 @@
+setwd("D:/projects/R_Projects/Sandbox/Stats_model_assignments") 
+getwd()
+
+
+load("04cars.rda")
+data = dat[,c(13,15)]
+names(data) = c("hp", "mpg")
+
+
+n = 100
+m = lm(y~x)
+p=1
+df_1 = p+1
+df_2=n-p-1
+fquartile = sqrt((p+1) * qf(conf, df_1,df_2))
+
+y_ = predict(m, data.frame(x=x), se =  T)
+
+
+cb_upper =  (y_$fit + ((fquartile)*y_$se.fit))
+cb_lower =  (y_$fit - ((fquartile)*y_$se.fit))
+
+plot(x,y, type = 'p')
+lines(x,cb_upper, type = 'l', col ="green")
+lines(x,cb_lower, type = 'l', col ="green")
+abline(m, col = 'red', lwd = 2)
+  
